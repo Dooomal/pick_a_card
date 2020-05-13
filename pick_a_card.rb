7c60eda7141b6ceeacdf710c2f1d4f3a39efb194
komok <komkov08@gmail.com>
2020-05-13 11:52:00 +0300

    added graf simbol

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,6 +2,5 @@
 
 # resgtggsrtgshthhgs5s56h56hxth
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suits = %w[Diamonds Hearts Clubs Spades]
-
-puts "#{values.sample} of #{suits.sample}"
+suits = %w[♤ ♡ ♢ ♧]
+puts "#{values.sample}#{suits.sample}"
