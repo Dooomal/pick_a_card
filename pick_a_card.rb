1aeeed1fbc077dff7e8d0986718d79dbfd5c44ff
komok <komkov08@gmail.com>
2020-05-13 11:29:31 +0300

    added textfile

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 # resgtggsrtgshthhgs5s56h56hxth
-values = ['2' ,  '3' ,  '4' ,  '5' ,  '6' ,  '7' ,  '8' ,  '9' ,  '10' ,  'J' ,  'Q' ,  'K' ,  'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+suits = %w[Diamonds Hearts Clubs Spades]
 
 puts "#{values.sample} of #{suits.sample}"
-
