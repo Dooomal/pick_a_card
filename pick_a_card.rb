04ab67d9f1c3acf23a0fba5b8e8bd4dcbe7d18aa
Dooomal <65237154+Dooomal@users.noreply.github.com>
2020-05-13 11:13:07 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,3 +1,4 @@
+# resgtggsrtgshthhgs5s56h56hxth
 values = ['2' ,  '3' ,  '4' ,  '5' ,  '6' ,  '7' ,  '8' ,  '9' ,  '10' ,  'J' ,  'Q' ,  'K' ,  'A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 
